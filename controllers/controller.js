fd7dd4e19e6206dd4811f18e7632590d8b2f1b4f
mikefillar <mikefillar21@gmail.com>
2024-06-26 11:40:02 +0800

    port added

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,7 @@ const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
   database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
